a6d32410d39ebc16811eb624033a972ca392d036
Endri-Tosku <endritosku001@gmail.com>
2025-12-16 16:02:08 +0100

    feat: inserito struttura base del js con le cards simili da modificare dopo

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Array di oggetti che rappresenta i membri del team
+// Ogni oggetto contiene tutte le informazioni necessarie
+// per costruire una card (nome, ruolo, email, immagine)
 const teamMembers = [
   {
     name: "Marco Bianchi",
@@ -36,3 +39,42 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+
+// Selezioniamo dal DOM il contenitore che ospiterà tutte le card
+// Questo elemento farà da "contenitore padre" per l'output dinamico
+const outputTest = document.querySelector(".team-container");
+
+// Variabile che servirà ad accumulare l'HTML delle card
+// Ad ogni giro del ciclo verrà aggiunta una nuova card come stringa
+let cards = "";
+
+// Cicliamo l'array teamMembers per creare una card per ogni membro
+for (let i = 0; i < teamMembers.length; i++) {
+
+  // A ogni iterazione estraiamo il singolo oggetto membro del team
+  const teamMember = teamMembers[i];
+
+  // Aggiungiamo una nuova card alla stringa "cards"
+  cards += `
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="./img/male1.png" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Marco Bianchi</h5>
+                        <p class="card-text">Designer</p>
+                        <p class="card-text"><small class="text-body-secondary">marcobianchi@team.com</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+}
+
+// Una volta terminato il ciclo
+// inseriamo tutta la stringa HTML accumulata nel contenitore
+// innerHTML trasforma la stringa in veri elementi HTML nel DOM
+outputTest.innerHTML = cards;
